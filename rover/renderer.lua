30e391e8b08e24fd0f492649e9389c9f04ebdb67
salty <rslnkssk@gmail.com>
2025-03-19 15:05:58 +0100

    images are good

--- rover/renderer.lua
+++ rover/renderer.lua
@@ -7,10 +7,18 @@ local Parser = require("rvparser")
 local Renderer = {}
 
 
-local tag_closers = {
-    ["text"] = "</p>",
+local tags = {
+    ["text"] = {
+        opener = "<p>",
+        closer = "</p>",
+    }
 }
 
+---create a href
+---@param start number
+---@param content string
+---@return string a resulting string
+---@return integer 0 if not a link, else an index in the content where ")" was found
 local function href(start, content)
     local end_href = 0
     local href_title = ""
@@ -36,11 +44,44 @@ local function href(start, content)
     return result, end_href
 end
 
+
+---create an img
+---@param start number
+---@param content string
+---@param last_token Token
+---@return string a resulting string
+---@return integer 0 if not an img, else an index in the content where ")" was found
+local function img(start, content, last_token)
+    local end_href = 0
+    local alt_text = ""
+    local href_link = ""
+    local link_start = 0
+    local result = ""
+    for i = start, #content do
+        if content:sub(i, i) == "]" then
+            alt_text = content:sub(start, i - 1)
+            i = i + 1
+            if content:sub(i, i) ~= "(" then
+                return "", 0
+            end
+            link_start = i + 1
+        end
+        if content:sub(i, i) == ")" then
+            end_href = i
+            href_link = content:sub(link_start, i - 1)
+            result = (tags[last_token.type] and tags[last_token.type].closer or "") ..
+                "<img src=\"" .. href_link .. "\" alt=\"" .. alt_text .. "\">"
+            break
+        end
+    end
+    return result, end_href
+end
+
 ---create heading
 ---@param token Token
 function Renderer:heading(token)
-    if self.last_token ~= nil and tag_closers[self.last_token.type] ~= nil then
-        self.content = self.content .. tag_closers[self.last_token.type]
+    if self.last_token ~= nil and tags[self.last_token.type].closer ~= nil then
+        self.content = self.content .. tags[self.last_token.type].closer
     end
     self.content = self.content .. "<h" .. token.count .. ">" .. token.content .. "</h" .. token.count .. ">"
 end
@@ -59,7 +100,8 @@ function Renderer:plain_text(token)
         return
     end
     local start = 1
-    for i = start, #token.content do
+    local i = start
+    while i < #token.content do
         if token.content:sub(i, i) == "[" then
             local result, endhref = href(i + 1, token.content)
             if endhref ~= 0 then
@@ -68,6 +110,20 @@ function Renderer:plain_text(token)
                 start = endhref + 1
             end
         end
+
+        if token.content:sub(i, i) == "!" and token.content:sub(i + 1, i + 1) == "[" then
+            local result, endhref = img(i + 2, token.content, self.last_token)
+            if endhref ~= 0 then
+                self.content = self.content .. token.content:sub(start, i - 1) .. result
+                i = endhref + 1
+                start = endhref + 1
+                if token.content:sub(i + 1, i + 1) ~= "" then
+                    self.content = self.content ..
+                    (tags[self.last_token.type] and tags[self.last_token.type].opener or "")
+                end
+            end
+        end
+        i = i + 1
     end
     self.content = self.content .. token.content:sub(start)
 end
--- rover/test_render.lua
+++ rover/test_render.lua
@@ -4,13 +4,15 @@ local test = {}
 
 test.renderer = function()
     local test_input = [[# Heading
-some [pure](bs) text hello world
+some [titel](link) text hello world
 multi line
 ## hello?
 more text
+![alttext](link) with some text
 ]]
     local expected =
-    "<h1>Heading</h1><p>some <a href=\"bs\">pure</a> text hello world multi line</p><h2>hello?</h2><p>more text</p>"
+        "<h1>Heading</h1><p>some <a href=\"link\">titel</a> text hello world multi line</p><h2>hello?</h2><p>more text </p>" ..
+        "<img src=\"link\" alt=\"alttext\"><p> with some text</p>"
     local result = Renderer:render(test_input)
     print(result)
     print(expected)
